9684764018818b54e01d12c603869bdbb2cc0933
Thomas-Radulesco <tom.radulesco@gmail.com>
2026-02-25 16:33:07 -0600

    FIXED some cart style

--- kibo-cart/src/pages/CartPage.tsx
+++ kibo-cart/src/pages/CartPage.tsx
@@ -20,44 +20,128 @@ const StyledDangerButton = styled(DangerButton)`
     }
 `;
 
-const Title = styled.div`
-    display: flex;
-    justify-content: space-between;
-    margin-top: 4rem;
-    margin-bottom: 2rem;
-`;
+
 
 const StyledSecondaryButton = styled(SecondaryButton)`
     && {
-        margin-bottom: 2rem;
+        // margin-bottom: 2rem;
+        margin-top: 2.5rem; // necessary when empty cart
+    }
+`;
+
+const StyledFooterButton = styled(SecondaryButton)`
+    && {
+        // margin-bottom: 2rem;
+        // margin-top: 2.5rem; // necessary when empty cart
     }
 `;
 
 const TotalCart = styled.h2`
-    margin-bottom: 6rem;
+    margin-bottom: 2rem;
     margin-top: 2rem;
 `;
 
 const ProductWrapper = styled.div`
     display: flex;
-    alignItems: center;
+    align-items: flex-start;
     gap: 1rem;
     border: 1px solid ${secondary.border};
     padding: 1rem;
+    width: 100%;
+    flex-wrap: wrap;
 `;
+
 
 const ProductDetails = styled.div`
     flex: 1;
 `;
 
 const ProductDetailsContainer = styled.div`
-    width: 100%;
+    flex: 1;
+    min-width: 0;
     display: flex;
     flex-direction: column;
+    gap: 0.5rem;
 `;
 
+
 const QuantityControlsWrapper = styled.div`
-    align-self: flex-end;
+    margin-left: auto;
+
+    @media (max-width: 500px) {
+        margin-left: 0;
+        align-self: flex-start;
+    }
+`;
+
+
+const CartFooter = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    margin-top: 1rem;
+    margin-bottom: 4rem;
+    @media (min-width: 600px) {
+        width: 90%;
+    }
+
+    @media (min-width: 900px) {
+        width: 80%;
+    }
+    
+    @media (min-width: 1200px) {
+        width: 70%;
+    }
+    
+    @media (min-width: 1300px) {
+        width: 60%;
+    }
+`;
+
+const EmptyCartInformation = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 7rem;
+    width: fit-content;
+    justify-content: space-between;
+    margin-top: 5rem;
+`;
+
+const CartWrapper = styled.div<{centered: boolean}>`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const CartHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    align-items: center;
+    margin-top: 4rem;
+    margin-bottom: 2rem;
+
+    @media (min-width: 600px) {
+        width: 90%;
+    }
+
+    @media (min-width: 900px) {
+        width: 80%;
+    }
+    
+    @media (min-width: 1200px) {
+        width: 70%;
+    }
+    
+    @media (min-width: 1300px) {
+        width: 60%;
+    }
+`;
+
+const StyledTitle = styled.h1`
+    margin-bottom: 2rem;
 `;
 
 export default function CartPage() {
@@ -70,16 +154,20 @@ export default function CartPage() {
 
   return (
     <div>
-        <Title>
-            <h1>Your Cart</h1>
-            {(items.length > 0) && (<StyledDangerButton onClick={clear}>Clear cart<StyledDeleteIcon /></StyledDangerButton>)}
-        </Title>
-        {(items.length === 0) && 
-            <p>Your cart is empty.</p>
-        }
-        <StyledSecondaryButton as={Link} to="/">
-            Back to Shop
-        </StyledSecondaryButton>
+    <CartWrapper centered={(items.length === 0) && true}>
+        {(items.length > 0) && (
+            <CartHeader>
+                <h1>Your Cart</h1>
+                <StyledDangerButton onClick={clear}>Clear cart<StyledDeleteIcon /></StyledDangerButton>
+            </CartHeader>
+        )}
+        {(items.length === 0) && (
+            <EmptyCartInformation>
+                <StyledTitle>Your Cart</StyledTitle>
+                <p>Your cart is empty.</p>
+                <StyledSecondaryButton as={Link} to="/">Back to Shop</StyledSecondaryButton>
+            </EmptyCartInformation>
+        )}
 
         {(items.length > 0) &&
             <>
@@ -87,8 +175,11 @@ export default function CartPage() {
                 <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
                     {items.map((item) => (
                     <ProductWrapper key={item.id}>
-                        <img src={item.image} alt={item.title} width={80} />
-
+                        <img
+                            src={item.image}
+                            alt={item.title}
+                            style={{ width: "80px", height: "auto", flexShrink: 0 }}
+                        />
                         <ProductDetailsContainer>
                             <ProductDetails style={{ flex: 1 }}>
                                 <h3>{item.title}</h3>
@@ -102,9 +193,13 @@ export default function CartPage() {
                     </ProductWrapper>
                     ))}
                 </div>
-                <TotalCart>Total: ${total.toFixed(2)}</TotalCart>
+                <CartFooter>
+                    <StyledFooterButton as={Link} to="/">Back to Shop</StyledFooterButton>
+                    <TotalCart>Total: ${total.toFixed(2)}</TotalCart>
+                </CartFooter>
             </>
         }
+    </CartWrapper>
     </div>
   );
 }
